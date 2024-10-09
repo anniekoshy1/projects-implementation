df8a3a3b7285c76a70a5d5a1c7557bdb340579ed
anniekoshy1 <koshya@email.sc.edu>
2024-10-09 16:35:57 -0400

    project

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -4,36 +4,20 @@ public class DataLoader {
 
     private ArrayList<User> users = new ArrayList<>();
 
-    public DataLoader() {
-        users.add(new User("user1", "password1"));
-        users.add(new User("user2", "password2"));
-        users.add(new User("user3", "password3"));
-        users.add(new User("user4", "password4"));
-        users.add(new User("user5", "password5"));
-        users.add(new User("user6", "password6"));
-        users.add(new User("user7", "password7"));
-        users.add(new User("user8", "password8"));
-        users.add(new User("user9", "password9"));
-    }
-
-    public ArrayList<User> getUserList() {
-        return users;
-    }
-
     public static void main(String[] args) {
-        DataLoader dataLoader = new DataLoader();
-        ArrayList<User> userList = dataLoader.getUserList();
+        // DataLoader dataLoader = new DataLoader();
+        // ArrayList<User> userList = dataLoader.getUserList();
 
-        for (int i = 0; i < Math.min(userList.size(), 8); i++) {
-            User user = userList.get(i);
-            System.out.println("Username: " + user.getUsername() + ", Password: " + user.getPassword());
-        }
+        // for (int i = 0; i < Math.min(userList.size(), 8); i++) {
+        //     User user = userList.get(i);
+        //     System.out.println("Username: " + user.getUsername() + ", Password: " + user.getPassword());
+        // }
 
-        User user2 = userList.get(1); 
-        System.out.println("\n--- Displaying User2 ---");
-        System.out.println("Username: " + user2.getUsername() + ", Password: " + user2.getPassword());
+        // User user2 = userList.get(1); 
+        // System.out.println("\n--- Displaying User2 ---");
+        // System.out.println("Username: " + user2.getUsername() + ", Password: " + user2.getPassword());
 
-        System.out.println("Program terminated.");
+        // System.out.println("Program terminated.");
     }
 
     public ArrayList<User> getUsers() {
--- src/TrueFalseQuestion.java
+++ src/TrueFalseQuestion.java
@@ -1,16 +1,13 @@
 public class TrueFalseQuestion extends Questions {
 
-    // Attributes
     private boolean correctAnswer;
     private boolean userAnswer;
 
-    // Constructor
     public TrueFalseQuestion(String question, boolean correctAnswer) {
-        super(question, Boolean.toString(correctAnswer), 0);  // Assuming difficulty is set to 0 for now
+        super(question, Boolean.toString(correctAnswer), 0); 
         this.correctAnswer = correctAnswer;
     }
 
-    // Methods
     public void submitAnswer(boolean userAnswer) {
         this.userAnswer = userAnswer;
     }
