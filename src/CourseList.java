ce44b1f46d2160926e7b3f9a76f4d658c66c67a8
anniekoshy1 <koshya@email.sc.edu>
2024-10-21 14:20:10 -0400

    course list

--- src/CourseList.java
+++ src/CourseList.java
@@ -2,16 +2,13 @@ import java.util.ArrayList;
 
 public class CourseList {
 
-    // Attributes
     private static CourseList instance;
     private ArrayList<Course> courses;
 
-    // Constructor
     private CourseList() {
         courses = new ArrayList<>();
     }
 
-    // Method to get the singleton instance
     public static CourseList getInstance() {
         if (instance == null) {
             instance = new CourseList();
@@ -19,28 +16,46 @@ public class CourseList {
         return instance;
     }
 
-    // Methods
     public Course addCourse(Course course) {
-        // No code for now
-        return null;
+        courses.add(course);
+        save(); 
+        return course;
     }
 
     public boolean removeCourse(Course course) {
-        // No code for now
-        return false;
+        boolean removed = courses.remove(course);
+        if (removed) {
+            save(); 
+        }
+        return removed;
     }
 
     public ArrayList<Course> getCourse(String courseName) {
-        // No code for now
-        return new ArrayList<>();
+        ArrayList<Course> foundCourses = new ArrayList<>();
+        for (Course course : courses) {
+            if (course.getName().equalsIgnoreCase(courseName)) {
+                foundCourses.add(course);
+            }
+        }
+        return foundCourses;
     }
 
     public Course findByName(String name) {
-        // No code for now
+        for (Course course : courses) {
+            if (course.getName().equalsIgnoreCase(name)) {
+                return course;
+            }
+        }
         return null;
     }
 
     public void save() {
-        // No code for now
+        DataWriter dataWriter = new DataWriter();
+        dataWriter.saveCourses(courses);
+    }
+
+    public void load() {
+        DataLoader dataLoader = new DataLoader();
+        courses = dataLoader.getCourses();
     }
 }
