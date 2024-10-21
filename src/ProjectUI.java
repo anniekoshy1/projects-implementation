057f7334091ba3f522634862084a9c6fed4343c4
anniekoshy1 <koshya@email.sc.edu>
2024-10-21 17:26:16 -0400

    phrase

--- src/ProjectUI.java
+++ src/ProjectUI.java
@@ -50,4 +50,48 @@ public class ProjectUI {
 		}
 		System.out.println("\n");
 	}
+
+    private int getUserCommand(int numCommands) {
+		System.out.print("What would you like to do?: ");
+		
+		String input = scanner.nextLine();
+		int command = Integer.parseInt(input) - 1;
+		
+		if(command >= 0 && command <= numCommands -1) return command;
+		
+		return -1;
+	}
+	
+	private void register() {
+		String userName = getField("Username");
+		String firstName = getField("First Name");
+		String lastName = getField("Last Name");
+		int age = Integer.parseInt(getField("Age"));
+		String phoneNumber = getField("Phone Number");
+		
+		if(library.createAccount(userName, firstName, lastName, age, phoneNumber)) {
+			System.out.println("You have successfully created an account");
+		} else {
+			System.out.println("Sorry an account with that username already exists");
+		}
+	}
+	
+	private void login() {
+		String userName = getField("Username");
+		
+		if(library.login(userName)) {
+			User currentUser = library.getCurrentUser();
+			System.out.println("Welcome " + currentUser.getFirstName() + " " + currentUser.getLastName() + "!");
+		} else {
+			System.out.println("Sorry, invalid username ");
+		}
+	}
+
+	
+	public static void main(String[] args) {
+		LibraryUI libraryInterface = new LibraryUI();
+		libraryInterface.run();
+
+	}
+
 }
